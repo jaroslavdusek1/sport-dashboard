b317ad71e0b150c8f8b2b8e6a8c6305f1b57e961
Anonymous <anonymous@example.com>
2025-02-11 01:58:06 +0100

    title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,17 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+    return (
+        <Html lang="en">
+            <Head>
+                <title>Sport Dashboard</title>
+                <meta name="description" content="Stay updated with the latest sports results!" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
 }
